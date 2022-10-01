6414033aae980fdf5719503ec51d1a5513eb35cb
cgodevs <kharol.go@gmail.com>
2022-09-30 23:08:11 -0300

    Add device button correctly functional

--- js/create-routine.js
+++ js/create-routine.js
@@ -1,3 +1,15 @@
+var todosDispositivos = ["Ar Condicionado", "Câmera", "Lâmpada",  "Rádio", "TV"];
+var dispositivosAindaDisponiveis = todosDispositivos.slice();
+var dispositivosSelecionados = [];
+var existeBotaoExcluir = false;
+var numeroDispositivos = 0;
+
+var criaBotaoExcluir = function(){
+    document.getElementById('botao_excluir').innerHTML = `<button onclick="excluiUltimoDispositivo()" id="botao_exclui_dispositivo" type="delete" class="offset-sm-1 btn btn-sm btn-danger mb-2">+</button>
+    <label for="botao_exclui_dispositivo">Excluir dispositivo</label>`;        
+    existeBotaoExcluir = true;    
+}
+
 var abreNovoCampoAcao = function(){
     var camposAcao = document.getElementById("campos_de_acao");
     var campoVisivel = camposAcao.lastElementChild; 
@@ -24,43 +36,52 @@ var abreNovoCampoAcao = function(){
     } ;
 }
 
-var todosDispositivos = ["Lâmpada", "TV", "Ar Condicionado", "Rádio", "Câmera"]
+var acrescentaLinhaHTMLDispositivo = function(){
+    var dispositivosHTML = "";
+    dispositivosAindaDisponiveis.forEach((d) => dispositivosHTML += `<option value="${dispositivosAindaDisponiveis.indexOf(d)}">${d}</option>`);
+    var novaLinhaDispositivo = `<div class="row" id="linha_dispositivo${dispositivosSelecionados.length}">                
+                                    <div class="form-group col-md-5 offset-md-1">
+                                        <label for="dispositivo"> 
+                                            ${dispositivosAindaDisponiveis.length} Dispositivos Disponíveis:
+                                        </label>
+                                        <select class="form-control" id="dispositivo${dispositivosSelecionados.length}">
+                                            ${dispositivosHTML} 
+                                        </select>
+                                    </div>
+                                    <div class="form-group col-md-5 mb-4">
+                                        <label for="acao">🔧Selecione uma ação:</label>
+                                        <select class="form-control" >
+                                        <option>Ligar</option>     
+                                        <option>Desligar</option>
+                                        </select>
+                                    </div>   
+                                </div>`;
+    document.getElementById('selecao_dispositivos').insertAdjacentHTML('beforeend', novaLinhaDispositivo);
+}
+acrescentaLinhaHTMLDispositivo();
 
-var contagemDispositivos = 0;
-var existeBotaoExcluir = false;
 var adicionarDispositivo = function(){
     event.preventDefault();
     if (!existeBotaoExcluir){
-        var novoBotaoExcluir = `<button onclick="excluiUltimoDispositivo()" id="botao_exclui_dispositivo" type="delete" class="offset-sm-1 btn btn-sm btn-danger mb-2">+</button>
-        <label for="botao_exclui_dispositivo">Excluir dispositivo</label>`;
-        document.getElementById('label_botao_seleciona_dispositivo').insertAdjacentHTML('afterend', novoBotaoExcluir)
-        existeBotaoExcluir = true;
+        //criaBotaoExcluir();
+    }
+    efetivarEscolha();
+    if(dispositivosAindaDisponiveis.length === 0){
+        document.getElementById("botao_seleciona_dispositivo").disabled = true;
+        return;
     } 
-
-    var dispositivosDisponiveis = "";
-    todosDispositivos.forEach((d) => dispositivosDisponiveis += ("<option>" + d +"</option>"));
-
-    var dispositivoNovo = `<div class="row"">                
-                                <div class="form-group col-md-10 offset-md-1">
-                                    <label for="dispositivo">(`+ (contagemDispositivos + 1) +`) Dispositivos Disponíveis:</label>
-                                    <select class="form-control">` +
-                                    dispositivosDisponiveis +
-                                    `</select>
-                                </div>
-                                <div class="form-group col-md-10 offset-md-1 mb-4">
-                                    <label for="acao">Selecione uma ação para o dispositivo escolhido:</label>
-                                    <select class="form-control" >
-                                    <option>Ligar</option>     
-                                    <option>Desligar</option>
-                                    </select>
-                                </div>      
-                            </div> `;
-    contagemDispositivos++;
-    document.getElementById('selecao_dispositivos').insertAdjacentHTML('beforeend', dispositivoNovo);
+    acrescentaLinhaHTMLDispositivo();
 }
+
+var efetivarEscolha = function(){   
+    var indiceDispositivo = parseInt(document.getElementById(`dispositivo${dispositivosSelecionados.length}`).value); 
+    var dispositivo = dispositivosAindaDisponiveis[indiceDispositivo]
+    dispositivosSelecionados.push(dispositivo);
+    dispositivosAindaDisponiveis.splice(indiceDispositivo, 1);
+}
+
 
 var excluiUltimoDispositivo = function(){
-    var filhos = document.getElementById("selecao_dispositivos");
-    filhos.removeChild(filhos[filhos.length - 1]);
-    contagemDispositivos--;
+
 }
+
