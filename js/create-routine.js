17250b049b0d0a7f632ea214943c061be841ec9a
cgodevs <kharol.go@gmail.com>
2022-10-02 18:09:06 -0300

    Pedro's review applied

--- js/create-routine.js
+++ js/create-routine.js
@@ -1,22 +1,21 @@
-var todosDispositivos = ["Ar Condicionado", "Câmera", "Lâmpada",  "Rádio", "TV"];
-var dispositivosAindaDisponiveis = todosDispositivos.slice();
-var dispositivosSelecionados = [];
-var existeBotaoExcluir = false;
-var numeroCamposDispositivos = 0;
+let dispositivosAindaDisponiveis = ["Ar Condicionado", "Câmera", "Caixa de Som" , "Lâmpada", "Rádio", "TV"];
+let dispositivosSelecionados = [];
+let existeBotaoExcluir = false;
+let numeroCamposDispositivos = 0;
 
-var abreNovoCampoAcao = function(){
-    var camposAcao = document.getElementById("campos_de_acao");
-    var campoVisivel = camposAcao.lastElementChild; 
+let abreNovoCampoAcao = function(){
+    let camposAcao = document.getElementById("campos_de_acao");
+    let campoVisivel = camposAcao.lastElementChild; 
         while (campoVisivel) {
             camposAcao.removeChild(campoVisivel);
             campoVisivel = camposAcao.lastElementChild;
         }
-    var acionamento = document.getElementById('tipo-acionamento').value;    
+    let acionamento = document.getElementById('tipo-acionamento').value;    
         if (acionamento == 'horario'){
             document.getElementById('campos_de_acao').innerHTML = `<div class="form-group col-md-10 offset-md-1 mb-4">
             <div class="row">
                 <div class="form-group col-md-8 mb-2">
-                    <label for="certa_distancia">⏰ Informe o horário do primeiro disparo:</label>
+                    <label for="certa_distancia">⏰ Informe o horário de disparo da rotina:</label>
                     <input type="datetime-local" class="form-control" id="certa_distancia" placeholder="">
                 </div>
                 <div class="form-group col-md-4">
@@ -34,27 +33,27 @@ var abreNovoCampoAcao = function(){
         </div>`
         } else if(acionamento == 'distancia'){
             document.getElementById('campos_de_acao').innerHTML = `<div class="form-group col-md-10 offset-md-1 mb-4">
-            <label for="certa_distancia">🛣️ Defina a distância (em metros) desejada de sua casa:</label>
+            <label for="certa_distancia">🛣️ Defina a que distância de sua casa a rotina deve ser acionada (em metros):</label>
             <input type="number" class="form-control" id="certa_distancia" placeholder="50">
         </div>`
         } else if(acionamento == 'minutos'){
             document.getElementById('campos_de_acao').innerHTML = `<div class="form-group col-md-10 offset-md-1 mb-4">
-            <label for="certa_distancia">⏱️ Defina a quantos minutos de sua casa:</label>
+            <label for="certa_distancia">⏱️ Defina a quanto tempo de sua casa a rotina deve ser acionada (em minutos)</label>
             <input type="number" class="form-control" id="certa_distancia" placeholder="2">
         </div>`
     } ;
 } 
 
-var criaBotaoExcluir = function(){ 
+let criaBotaoExcluir = function(){ 
     document.getElementById('botao_excluir').innerHTML = `<button onclick="excluiUltimoDispositivo()" id="botao_exclui_dispositivo" type="delete" class="offset-sm-1 btn btn-sm btn-danger mb-2">×</button>
     <label for="botao_exclui_dispositivo" id="label_botao_excluir">Excluir dispositivo</label>`;        
     existeBotaoExcluir = true;    
 } 
 
-var criaHTMLCamposDispositivos = function(){ // cria HTML para campos de dispositivos com a lista de dispositivosSelecionados
-    var dispositivosHTML = "";
+let criaHTMLCamposDispositivos = function(){ // cria HTML para campos de dispositivos com a lista de dispositivosSelecionados
+    let dispositivosHTML = "";
     dispositivosAindaDisponiveis.forEach((d, index) => dispositivosHTML += `<option value="${index}">${d}</option>`);
-    var linhaHTML = `<div class="row" id="linha_dispositivo${numeroCamposDispositivos}">                
+    let linhaHTML = `<div class="row" id="linha_dispositivo${numeroCamposDispositivos}">                
                                     <div class="form-group col-md-5 offset-md-1">
                                         <label for="dispositivo"> 
                                             ${dispositivosAindaDisponiveis.length} Dispositivos Disponíveis:
@@ -74,14 +73,14 @@ var criaHTMLCamposDispositivos = function(){ // cria HTML para campos de disposi
     return linhaHTML;
 } 
 
-var acrescentaCamposDispositivos = function(){ // monta HTML com todas as opções de dispositivos
+let acrescentaCamposDispositivos = function(){ // monta HTML com todas as opções de dispositivos
     numeroCamposDispositivos++;
-    var campos = criaHTMLCamposDispositivos();
+    let campos = criaHTMLCamposDispositivos();
     document.getElementById('selecao_dispositivos').insertAdjacentHTML('beforeend', campos);    
 } 
 acrescentaCamposDispositivos(); // já inicia a página com os campos para selecionar 1 dispositivo
 
-var adicionarDispositivo = function(){
+let adicionarDispositivo = function(){
     event.preventDefault();
     if (!existeBotaoExcluir){
         criaBotaoExcluir();
@@ -94,15 +93,15 @@ var adicionarDispositivo = function(){
     acrescentaCamposDispositivos();
 } 
 
-var efetivarEscolha = function(){   
-    var indiceDispositivo = parseInt(document.getElementById(`dispositivo${numeroCamposDispositivos - 1}`).value); 
-    var dispositivo = dispositivosAindaDisponiveis[indiceDispositivo];
+let efetivarEscolha = function(){   
+    let indiceDispositivo = parseInt(document.getElementById(`dispositivo${numeroCamposDispositivos - 1}`).value); 
+    let dispositivo = dispositivosAindaDisponiveis[indiceDispositivo];
     dispositivosSelecionados.push(dispositivo);
     dispositivosAindaDisponiveis.splice(indiceDispositivo, 1);
 }
 
 
-var excluiUltimoDispositivo = function(){
+let excluiUltimoDispositivo = function(){
     event.preventDefault();   
     if(dispositivosSelecionados.length > 0 && numeroCamposDispositivos != 1){      
         document.getElementById(`linha_dispositivo${numeroCamposDispositivos}`).remove();
